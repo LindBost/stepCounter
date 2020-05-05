670ef62f392c81fcc2c7e93bf9746fad10318958
sonifern <sonia.fernandes.pereira@tele2.com>
2020-05-05 09:07:53 +0200

    first commit

---
+++ src/main/java/com/example/stepupservice/StepUpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.stepupservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepUpServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StepUpServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stepupservice/StepUpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stepupservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepUpServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
