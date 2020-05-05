0ad1f3023c2a1fd995bb445ae293de5969ced29c
Therese Göransson <therese.goransson@tele2.com>
2020-05-05 10:06:20 +0200

    wip

--- src/main/java/com/example/stepupservice/api/UserController.java
+++ src/main/java/com/example/stepupservice/api/UserController.java
@@ -2,6 +2,7 @@ package com.example.stepupservice.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @PostMapping("/login")
-    public ResponseEntity<> login() {
-
+    public ResponseEntity<Void> login(@RequestBody UserRequest userRequest) {
+    return ResponseEntity.ok(null);
     }
 }
--- src/main/java/com/example/stepupservice/api/UserRequest.java
+++ src/main/java/com/example/stepupservice/api/UserRequest.java
@@ -1,4 +1,18 @@
 package com.example.stepupservice.api;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserRequest {
+
+    public UserRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    private String email;
+    private String password;
+
 }
