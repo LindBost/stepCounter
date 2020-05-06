5aef7705b0a4ddd81187edbb9e967ccca85697e3
lindbost <linda.bostrom@tele2.com>
2020-05-06 16:12:40 +0200

    update datepicker

--- frontend/src/components/StepTracker.js
+++ frontend/src/components/StepTracker.js
@@ -1,6 +1,10 @@
 import React, {useState} from 'react';
 import {login, updateUserSteps} from "../service/UserService";
-import DatePicker from "react-datepicker/es";
+import DatePicker, { registerLocale } from "react-datepicker";
+import sv from "date-fns/locale/sv"; // the locale you want
+import moment from 'moment';
+import format from "date-fns/format";
+registerLocale("sv", sv);
 
 
 const StepTracker = ({userInfo, fetchMySteps}) => {
@@ -8,6 +12,11 @@ const StepTracker = ({userInfo, fetchMySteps}) => {
     const [steps, setSteps] = useState('');
     const [date, setDate] = useState(null);
 
+    const test = (date) => {
+
+        setDate(format(date, "yyyy-MM-dd"));
+
+    }
     const handleSteps = async () => {
 
         const personalData = {
@@ -15,7 +24,7 @@ const StepTracker = ({userInfo, fetchMySteps}) => {
             steps: steps,
             date: date
         };
-
+        console.log('PERS', personalData)
         await updateUserSteps(personalData);
         fetchMySteps();
     };
@@ -23,7 +32,7 @@ const StepTracker = ({userInfo, fetchMySteps}) => {
     return (
         <>
             <input placeholder="Steps" type="text" value={steps} onChange={(event) => setSteps(event.target.value)}/>
-            <DatePicker selected={date} onChange={(date) => setDate(date)} />
+            <DatePicker locale={sv} selected={Date.parse(date)} onChange={test} dateFormat="yyyy-MM-dd"/>
             <button onClick={handleSteps}> Update your information </button>
         </>
     )
