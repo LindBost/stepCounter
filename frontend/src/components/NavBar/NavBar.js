5956cf1d075f0f4cc5cd0c67591d5cb6285f3ac4
Therese Göransson <therese.goransson@tele2.com>
2020-05-12 11:16:23 +0200

    Just some css and moving

---
+++ frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NavBar.css';
+
+const logo = require('../../pic/logga.png');
+const NavBar = () => {
+
+    return (
+        <div className='navbar'>
+            <div className="logo">
+                <img src={logo}/>
+            </div>
+            <div className="menu">
+                <h1>StepUp </h1>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
