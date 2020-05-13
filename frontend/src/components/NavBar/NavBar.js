a49227742bfd80b3683e9215ffafe64070dcd916
Therese Göransson <therese.goransson@tele2.com>
2020-05-13 13:22:32 +0200

    css

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -5,12 +5,15 @@ const logo = require('../../pic/logga.png');
 const NavBar = () => {
 
     return (
-        <div className='navbar'>
+        <div className="navbar">
             <div className="logo">
                 <img src={logo}/>
             </div>
             <div className="menu">
                 <h1>StepUp </h1>
+                <ul className='navbar-items'>
+
+                </ul>
             </div>
         </div>
     );
